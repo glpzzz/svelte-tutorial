7e993e5159a48931b8ca5ade4f659d8996d528b9
Gabriel A. López López <glpz@daxslab.com>
2022-07-08 23:11:45 -0400

    introduce reactive values and statements

--- src/App.svelte
+++ src/App.svelte
@@ -1,43 +1,54 @@
 <script>
-	let name = "Yoshi";
-	let beltColour = "black";
-	let otherProperty  = 'property value'
+    let firstName = "Jimi";
+    let lastName = "Hendrix";
+    let beltColour = "black";
 
-	const handleClick = () => {
-		beltColour = 'orange';
-	}
+    // reactive values with $:
+    $: fullName = `${firstName} ${lastName}`;
 
-	const handleInput = (e) => {
-		beltColour = e.target.value;
-	}
+    // reactive statements
+    $: console.log(beltColour);
+
+    // reactive code block
+    $: {
+        console.log(beltColour);
+        console.log(fullName);
+    }
+
+    const handleClick = () => {
+        beltColour = 'orange';
+    }
+
+    const handleInput = (e) => {
+        beltColour = e.target.value;
+    }
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p style="color:{beltColour}">{beltColour} belt. Property value = {otherProperty}.</p>
-	<button on:click={handleClick}>update belt colour</button>
-	<input type="text" on:input={handleInput} value="{beltColour}">
-	<input type="text" bind:value="{otherProperty}">
+    <p style="color:{beltColour}">{fullName} has a {beltColour} belt.</p>
+    <input type="text" bind:value={beltColour}>
+    <input type="text" bind:value={firstName}>
+    <input type="text" bind:value={lastName}>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+    main {
+        text-align: center;
+        padding: 1em;
+        max-width: 240px;
+        margin: 0 auto;
+    }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+    h1 {
+        color: #ff3e00;
+        text-transform: uppercase;
+        font-size: 4em;
+        font-weight: 100;
+    }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+    @media (min-width: 640px) {
+        main {
+            max-width: none;
+        }
+    }
 </style>
